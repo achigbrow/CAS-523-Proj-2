f1576ef606548ebaf84e72fa454ee726662c4ba7
achigbrow <achigbrow@unm.edu>
2023-03-12 14:59:38 -0600

    de-duplicates and other adjustments

--- src/mutate.py
+++ src/mutate.py
@@ -8,93 +8,135 @@ from tabulate import tabulate
 from utils import get_mutated_phenotype
 
 
-def mutation(original_phenotype, genome, generation, id, pos):
-    """
-    Takes a string genome and returns a dataframe containing its mutations.
-    generation=0 will build a dataframe containing generation 1 with 1
-    mutation per string and mut_pos no nucleotides because pos will be an empty
-    array. generation=1 will build a df containing generation 2 with 2
-    mutations per string but will skip the nucleotide site listed in pos.
-    And generation=2  will generate the third generation with three mutations
-    per string.
-    :param genome: a string of the genome
-    :param generation: int representation of generation of the genome being
-    passed with 0 being original
-    strain and each subsequent iteration indicating number of mutations in
-    the generation. [0,2]
-    :param id: id associated with this parent genome
-    :param pos: array containing int val(s) to skip when mutating as they
-    are already mutated
-    generation 0 should be [], gen 1 should be
-    :return: dataframe with cols: id, genome, generation, parent (id), mut_pos
-    """
+def mutation(original_phenotype, original_genotype, genome_df):
 
-    df = pd.DataFrame(columns=["id", "genome", "generation", "parent", "mut_pos", "mutated_protein_index"])
+    next_gen = pd.DataFrame(
+        columns=[
+            "id",
+            "genome",
+            "generation",
+            "parent",
+            "mut_pos",
+            "mutated_protein_index",
+        ]
+    )
     nucleotides = ["A", "C", "G", "T"]
     df_loc = 0
+    total_revert = 0
+    total_dupes = 0
 
-    for i in range(len(genome)):
-        if genome[i] not in nucleotides:
-            print("ERROR: Unexpected nucleotide value: ", genome[i])
-            sys.exit(1)
+    for row in genome_df.itertuples(index=False):
+        # get the parent genome to mutate
+        genome = row[1]
+        # get where the parent genome has been previously mutated
+        pos = row[4]
+        # get the parent genome's generatation
+        generation = row[2]
+        # get the parent genome's id
+        id = row[0]
 
-        if i not in pos:
-            if pos == []:
-                mut_pos = [i]
-            else:
-                mut_pos = pos[:]
-                mut_pos.append(i)
+        # mutation
+        for i in range(len(genome)):
+            if genome[i] not in nucleotides:
+                print("ERROR: Unexpected nucleotide value: ", genome[i])
+                sys.exit(1)
+            # identify where the genome was mutated previously
 
-            for nuc in nucleotides:
-                # only create a mutation if it is different from original
-                if genome[i] is not nuc:
-                    # creates the (mut)ated genome
-                    if i == 0:
-                        mut = nuc + genome[1:]
-                    elif i == len(genome):
-                        mut = genome[: len(genome)] + nuc
-                    else:
-                        mut = genome[:i] + nuc + genome[i + 1 :]
-                    # id for this genome, the genome, the generation of the
-                    # genome, and its parent's id
-                    temp = [uuid.uuid1().int, mut, generation + 1, id, mut_pos, None]
-                    temp = get_mutated_phenotype(original_phenotype, temp)
-                    df.loc[df_loc] = temp
-                    df_loc += 1
-    return df
+            if i not in pos:
+                if not pos:
+                    mut_pos = [i]
+                else:
+                    mut_pos = pos[:]
+                    mut_pos.append(i)
+
+                for nuc in nucleotides:
+                    # only create a mutation if it is different from original
+                    if genome[i] is not nuc:
+                        # creates the (mut)ated genome
+                        if i == 0:
+                            mut = nuc + genome[1:]
+                        elif i == len(genome):
+                            mut = genome[: len(genome)] + nuc
+                        else:
+                            mut = genome[:i] + nuc + genome[i + 1 :]
+
+                        # check if mutation is reversion and don't add if it is
+                        if mut == original_genotype:
+                            total_revert += 1
+                            continue
+
+                        if mut in next_gen["genome"].values:
+                            parent_list = (
+                                next_gen[next_gen.genome == mut].parent.item() + ", "
+                                "" + id
+                            )
+                            next_gen.loc[next_gen.genome == mut, "parent"] = parent_list
+                            total_dupes += 1
+                            continue
+
+                        # id for this genome, the genome, the generation of the
+                        # genome, and its parent's id
+                        temp = [
+                            str(uuid.uuid1().int),
+                            mut,
+                            generation + 1,
+                            str(id),
+                            mut_pos,
+                            None,
+                        ]
+                        temp = get_mutated_phenotype(original_phenotype, temp)
+                        next_gen.loc[df_loc] = temp
+                        df_loc += 1
+    print("total reversions: ", total_revert)
+    print("total duplicates: ", total_dupes)
+    return next_gen
 
 
 def test_mutation():
-    orig = "AAAAAAAAA"  # Phenotype: MKA
-    orig_id = uuid.uuid1().int
+    # orig = "AAAAAAAAA"  # Phenotype: MKA
+    orig = "TACCATGGAATTACTGCG"  # Phenotype: YHGITA
+    orig_id = str(uuid.uuid1().int)
 
     original_phenotype = Seq(orig).translate()
     print(f"\n\noriginal_phenotype: {original_phenotype}")
+    gen0 = pd.DataFrame(
+        columns=[
+            "id",
+            "genome",
+            "generation",
+            "parent",
+            "mut_pos",
+            "mutated_protein_index",
+        ]
+    )
+    gen0.loc[0] = [orig_id, orig, 0, "none", [], [-1]]
 
-    gen1 = mutation(original_phenotype, orig, 0, orig_id, [])
-    print(tabulate(gen1, headers="keys", tablefmt="psql"))
-    print(gen1.shape)
+    gen1 = mutation(original_phenotype, orig, gen0)
+    neutral1 = gen1.loc[gen1["mutated_protein_index"].isin([[-1]])]
+    print(neutral1.shape)
 
-    gen2_list = []
-
-    for row in gen1.itertuples(index=False):
-        gen2_list.append(mutation(original_phenotype, row[1], row[2], row[0], row[4]))
-
-    gen2 = pd.concat(gen2_list)
-    gen2 = gen2.drop_duplicates(subset=["genome"])
-    print(tabulate(gen2, headers="keys", tablefmt="psql"))
+    gen2 = mutation(original_phenotype, orig, neutral1)
+    # print(tabulate(gen2, headers="keys", tablefmt="psql"))
     print(gen2.shape)
 
-    gen3_list = []
+    neutral2 = gen2.loc[gen2["mutated_protein_index"].isin([[-1]])]
+    print(neutral2.shape)
 
-    for row in gen2.itertuples(index=False):
-        gen3_list.append(mutation(original_phenotype, row[1], row[2], row[0], row[4]))
-
-    gen3 = pd.concat(gen3_list)
-    gen3 = gen3.drop_duplicates(subset=["genome"])
-    print(tabulate(gen3, headers="keys", tablefmt="psql"))
+    gen3 = mutation(original_phenotype, orig, neutral2)
+    # print(tabulate(gen3, headers="keys", tablefmt="psql"))
     print(gen3.shape)
-    print("done")
+
+    neutral3 = gen3.loc[gen3["mutated_protein_index"].isin([[-1]])]
+    # print(tabulate(neutral3, headers="keys", tablefmt="psql"))
+    print(neutral3.shape)
+
+    gen4 = mutation(original_phenotype, orig, neutral3)
+    # print(tabulate(gen3, headers="keys", tablefmt="psql"))
+    print(gen4.shape)
+
+    neutral4 = gen4.loc[gen4["mutated_protein_index"].isin([[-1]])]
+    # print(tabulate(neutral3, headers="keys", tablefmt="psql"))
+    print(neutral4.shape)
 
 
 if __name__ == "__main__":
