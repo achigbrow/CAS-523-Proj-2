fb9f2797ef367f6db80922b3d8c153ee44002522
Jason Stewart <jason00stewart@gmail.com>
2023-03-27 14:13:41 -0600

    plots and saves titer for 30-node nn

--- src/neutral_network/driver.py
+++ src/neutral_network/driver.py
@@ -7,17 +7,17 @@ from antigenic_neutral_network import AntigenicNeutralNetwork
 def main():
     # Neutral network params
     tolerance = 0.95
-    size = 2000
+    size = 30
 
     # Init the antigenically neutral network
     ann = AntigenicNeutralNetwork(size=size, tolerance=tolerance)
 
-    ann.build(to_print=False, only_mutate_neutral=False)
+    ann.build(to_print=True, only_mutate_neutral=True)
     ann.make_titer_and_distance_tables()
     path = "C:/Users/jason/AppData/Local/R/win-library/4.2/Racmacs/extdata/titer.csv"
     ann.save_titer_table(path)
 
-    #ann.create_figure()
+    ann.create_figure()
 
     # for node in ann.nodes.keys():
     #     print(ann.nodes[node])
